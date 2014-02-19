cf7e16f5a5603ed4bb1b858c75857fb73e8f8ef6
Philip McMahon <13phil13@gmail.com>
2014-02-19 17:28:05 +0000

    Added south, setup database (separate config file)

--- benpledge/benpledge/settings.py
+++ benpledge/benpledge/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,10 @@ WSGI_APPLICATION = 'benpledge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS' : {
+            'read_default_file': '/home/phil/benpledge/dbconfig.cnf',
+        },
     }
 }
 
