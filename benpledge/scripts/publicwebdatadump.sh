141c7987a6028ea184160ab525a8458d83306651
Philip McMahon <13phil13@gmail.com>
2014-05-09 21:14:16 +0100

    Auth dump added to script

--- benpledge/scripts/publicwebdatadump.sh
+++ benpledge/scripts/publicwebdatadump.sh
@@ -1,3 +1,4 @@
 workon benpledge_ve
-../manage.py dumpdata publicweb --exclude publicweb.hatresultsdatabase --exclude publicweb.houseidlookup --exclude publicweb.lsoadomesticenergyconsumption --exclude publicweb.postcodeoalookup --exclude publicweb.pledge --exclude publicweb.ecoeligible --format json --indent 4 > /home/phil/benpledge/benpledge/publicweb/fixtures/publicweb_test_fixture.json
+../manage.py dumpdata publicweb --exclude publicweb.hatresultsdatabase --exclude publicweb.houseidlookup --exclude publicweb.lsoadomesticenergyconsumption --exclude publicweb.postcodeoalookup --exclude publicweb.pledge --exclude publicweb.ecoeligible --format json --indent 4 > ../publicweb/fixtures/publicweb_test_fixture.json
 ../manage.py dump_test_fixtures
+../manage.py dumpdata auth > ../publicweb/fixtures/auth.json
